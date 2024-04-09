fbc95c3348a20100df7636577ca9335dc0ef5290
Beth <beth.williams@bluewin.ch>
2024-04-09 16:06:11 +0200

    feat: finds highest prime in 5 seconds

--- src/com/appisberg/prime/Prime.java
+++ src/com/appisberg/prime/Prime.java
@@ -3,7 +3,7 @@ package com.appisberg.prime;
 import java.util.ArrayList;
 
 /**
-* This program will print as many prime numbers as possible in 60 seconds.
+* This program will find the highest prime number it can within 5 seconds.
 *
 * @author Beth Williams
 * @version 1.0
@@ -12,7 +12,7 @@ public class Prime {
     public static void main(String[] args) {
         ArrayList<Integer> primes = new ArrayList<>();       //array list to store any prime numbers found
         primes.add(2);                                       //include '2' as default
-        System.out.println(2);
+        //System.out.println(2);
 
         boolean factor = false;     //in case extant factor
         int i = 3;                  //start searching from 3
@@ -32,13 +32,14 @@ public class Prime {
             //add i to list and print if no factor exists
             if (!factor) {
                 primes.add(i);
-                System.out.println(i);
+                //System.out.println(i);
             }
 
             factor = false;     //unset boolean
             i += 2;             //skip even numbers
 
             elapsedTime = System.currentTimeMillis() - startTime;       //update time elapsed
-        } while (elapsedTime < 60*1000);                                //run for 1 minute
+        } while (elapsedTime < 5*1000);                                //run for 5 seconds
+	System.out.println("Highest prime number found in 5 seconds: " + primes.get(primes.size()-1)); 
     }
 }
